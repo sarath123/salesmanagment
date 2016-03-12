f05dfa7eec649bc7c62430b5ac60e0528a51f983
sarath123 <sarathsajeev1995@gmail.com>
2016-03-12 22:54:51 +0530

    sync again

--- HTML1/config.php
+++ HTML1/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $host='localhost';
@@ -10,17 +9,4 @@ $link=mysql_connect($host,$user,$pass);
 
 $con=mysql_select_db($dbname,$link);
 
-=======
-<?php
-
-$host='localhost';
-$user='root';
-$pass='';
-$dbname='salesmanagement';
-
-$link=mysql_connect($host,$user,$pass);
-
-$con=mysql_select_db($dbname,$link);
-
->>>>>>> 4517c1e9d0176b17e76e3fb2f6ba30987c0f3c59
 ?>
--- HTML1/studyphp.php
+++ HTML1/studyphp.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 echo 'Hello World<br>hello all';
 
@@ -15,22 +14,4 @@ echo '<br>'.$result.'<br>';
 echo " My name is $name. And my age is $age";
 
 
-=======
-<?php
-echo 'Hello World<br>hello all';
-
-$name="Sarath";
-$age=20;
-
-$num1=1;
-$num2=2;
-
-$result=$num1+$num2;
-
-echo '<br>'.$result.'<br>';
-
-echo " My name is $name. And my age is $age";
-
-
->>>>>>> 4517c1e9d0176b17e76e3fb2f6ba30987c0f3c59
 ?>
